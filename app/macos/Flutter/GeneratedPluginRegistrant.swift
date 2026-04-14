d7ee55ad8ad24d63fa9f40a30257c8f470fecdad
Cedric Ziel <cedric@cedric-ziel.com>
2026-04-14 23:00:46 +0200

    fix(app): upgrade record to v6, add microphone entitlements for macOS and iOS

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import audioplayers_darwin
 import flutter_local_notifications
 import flutter_secure_storage_darwin
 import package_info_plus
-import record_darwin
+import record_macos
 import screen_retriever_macos
 import shared_preferences_foundation
 import tray_manager
@@ -21,7 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
